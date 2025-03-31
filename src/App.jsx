02601f22f60b0660ae38b8aac9cc09a31a82de37
Noxx-design <rm94529@estudante.fieb.edu.br>
2025-03-31 08:32:45 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,41 @@ import { useState } from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('???')
+  
+
+  const Formulario = (props) => {
+    
+    const [nome, setNome] = useState()
+
+    return (
+      <div>
+        <h3>{props.titulo}</h3>
+        <input className="nome"
+        placeholder="Digite seu nome..."
+        onChange={(e)=>{setNome(e.target.value)}}
+        name="nome"
+        type="text" />
+        <button className="botao" onClick={
+                ()=> { 
+                  if(nome.length > 0)
+                    alert(nome)
+                  else
+                    alert(props.mensagen)
+            }
+          }>
+         CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
 
   return(
     <div>
       <h3>Pizzaria 2B</h3>
-      <input className="nome"
-      placeholder="Digite seu nome..."
-      onChange={(e)=>{setNome(e.target.value)}}
-      name="nome"
-       type="text" />
-       <button className="botao" onClick={()=>alert(nome)}>
-        CLIQUE AQUI
-       </button>
+      <Formulario titulo = "Nome"  mensagen = "Nos vamos presisar do seu Nome"/>
+      <Formulario titulo = "E-mail" mensagen = "Nos vamos presisar do seu E-mail"/>
+      <Formulario titulo = "CPF" mensagen = "Nos vamos presisar do seu CPF"/>
+      <Formulario titulo = "Telefone" mensagen = "Nos vamos presisar do seu Telefone"/>
     </div>
   )
 }
